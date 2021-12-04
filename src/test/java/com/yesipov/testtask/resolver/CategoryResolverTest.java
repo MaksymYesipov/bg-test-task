0a513c9375b019db2b47ad3429a74fc21e69f451
Maksym_Yesipov <esipov02@gmail.com>
2021-12-04 12:05:00 +0200

    [BG_Test]: Level 2 started

---
+++ src/test/java/com/yesipov/testtask/resolver/CategoryResolverTest.java
@@ -0,0 +1,20 @@
+package com.yesipov.testtask.resolver;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class CategoryResolverTest {
+
+    private CategoryResolver resolver = new CategoryResolver();
+
+    @Test
+    void resolveCategories() {
+        resolver.
+    }
+
+    @Test
+    void getResults() {
+    }
+}
